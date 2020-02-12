58c18f610e8f27087cf9b9615730266d03ab0b66
davida2 <andrada.david@ppb.com>
2020-02-12 11:26:22 +0200

    cookies and refresh

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,4 +1,87 @@
 $(function() {
+
+    function setCookie(name,value,days) {
+        var expires = "";
+        if (days) {
+            var date = new Date();
+            date.setTime(date.getTime() + (days*24*60*60*1000));
+            expires = "; expires=" + date.toUTCString();
+        }
+        document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+    }
+    function getCookie(name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for(var i=0;i < ca.length;i++) {
+            var c = ca[i];
+            while (c.charAt(0)==' ') c = c.substring(1,c.length);
+            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+        }
+        return null;
+    }
+
+
+    // if (getCookie('CookieShow') == null) {
+    //     $('body').addClass('fixed');
+    //
+    //     $('body').bsgdprcookies({
+    //
+    //         id: 'cookie-modal',
+    //         class: '',
+    //         title: "Flo Adventure foloseste cookie-uri",
+    //         backdrop: 'static',
+    //         message: 'Acest site folosește cookie-uri. Continuarea navigării reprezintă acceptul dvs. pentru această folosință. Pentru mai multe detalii privind gestionarea preferințelor privind cookie-uri, vedeți Politica de utillizare cookie-uri',
+    //         messageMaxHeightPercent: 25,
+    //         delay: 1500,
+    //         expireDays: 30,
+    //         moreLinkActive: true,
+    //         moreLinkLabel: 'More informations..',
+    //         moreLinkNewTab: true,
+    //         moreLink: 'privacy-policy.php',
+    //         acceptButtonLabel: 'Accept',
+    //         allowAdvancedOptions: true,
+    //         advancedTitle: 'Select which cookies you want to accept',
+    //         advancedAutoOpenDelay: 1000,
+    //         advancedButtonLabel: 'Customize',
+    //         advancedCookiesToSelect: [
+    //             {
+    //                 name: 'necessary',
+    //                 title: 'Necessary',
+    //                 description: 'Required for the site to work properly',
+    //                 isFixed: true
+    //             },
+    //             {
+    //                 name: 'preferences',
+    //                 title: 'Site Preferences',
+    //                 description: 'Required for saving your site preferences, e.g. remembering your username etc.',
+    //                 isFixed: false
+    //             },
+    //             {
+    //                 name: 'analytics',
+    //                 title: 'Analytics',
+    //                 description: 'Required to collect site visits, browser types, etc.',
+    //                 isFixed: false
+    //             },
+    //             {
+    //                 name: 'marketing',
+    //                 title: 'Marketing',
+    //                 description: 'Required to marketing, e.g. newsletters, social media, etc',
+    //                 isFixed: false
+    //             }
+    //         ],
+    //         OnAccept: function () {
+    //             $('body').removeClass('fixed');
+    //         }
+    //     });
+    // } else {
+    //     $('body').removeClass('fixed');
+    // }
+
+
+
+
+
+
     if  ($("#demo-grid").length) {
         var grid = $("#demo-grid");
         var asyncGrid = new Gridifier(grid, {
