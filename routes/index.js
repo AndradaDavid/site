4319e517d9448bb1ba2242771fd90abb9efa01a5
davida2 <andrada.david@ppb.com>
2020-12-16 16:31:52 +0200

    apuseni 4x4

--- routes/index.js
+++ routes/index.js
@@ -88,7 +88,7 @@ exports.patru3 = function(req, res){
     res.render('patru3',
         {
             i18n: res,
-            'lang': 'ro',
+            'lang': req.cookies.i18n,
             'device': getDevice(req),
             'type': getDeviceType(req),
             'navigation':'romania'
